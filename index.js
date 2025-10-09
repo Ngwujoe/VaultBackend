41b718b4880a148d60b9bced87c5e0e0c672b08f
Joe <ngwujoseph90@gmail.com>
2025-10-09 15:03:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const logWithTime = (msg, data = null) => {
 // MongoDB Connection
 // -------------------------
 mongoose
-  .connect("mongodb://127.0.0.1:27017/vaultDB", {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
